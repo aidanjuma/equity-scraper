a30fbf3a959fd07cae8d516bbed762c7c5ff4a67
Aidan Juma <aidan@aidanjuma.dev>
2022-12-27 21:40:28 +0000

    📃 define and add data types

---
+++ src/models/index.ts
@@ -0,0 +1,23 @@
+import {
+  IAsset,
+  IGoogleFinanceAsset,
+  IPriceRange,
+  IMarketSummary,
+  INewsArticle,
+  IBinanceAsset,
+  AssetType,
+  Market,
+  Currency,
+} from "./types";
+
+export {
+  IAsset,
+  IGoogleFinanceAsset,
+  IPriceRange,
+  IMarketSummary,
+  INewsArticle,
+  IBinanceAsset,
+  AssetType,
+  Market,
+  Currency,
+};
