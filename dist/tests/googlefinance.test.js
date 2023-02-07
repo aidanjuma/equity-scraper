34e5edce08c54966747ba70db6251bdea99c5b55
Aidan Juma <aidan@aidanjuma.dev>
2023-02-07 19:36:10 +0000

    🧪 improved testing + added more tests

--- dist/tests/googlefinance.test.js
+++ dist/tests/googlefinance.test.js
@@ -6,28 +6,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const googlefinance_1 = __importDefault(require("../../src/providers/fiat/googlefinance"));
 jest.setTimeout(120000);
 // run: yarn test --watch --verbose false googlefinance.test.ts
-// TODO: Add more and improve current tests.
 describe("GoogleFinance Class", () => {
+    const googleFinance = new googlefinance_1.default();
+    let offsetTestList;
+    test("Test: Pagination - limit = 20, so list length should be 20.", async () => {
+        const assets = await googleFinance.getAvailableAssets(20);
+        console.log(assets);
+        offsetTestList = assets;
+        expect(assets).toHaveLength(20);
+    });
+    test("Test: Pagination - offset = 5, limit = 15, so list length should be 15 and the new list's first element should be the same as the previous' 5th index.", async () => {
+        const assets = await googleFinance.getAvailableAssets(15, 5);
+        console.log(assets);
+        expect(assets).toHaveLength(15);
+        expect(assets[0]).toEqual(offsetTestList[5]);
+    });
     test("Test: Get asset data from asset META:NASDAQ (stock).", async () => {
-        const googleFinance = new googlefinance_1.default();
         const assetData = await googleFinance.getAssetData("META:NASDAQ");
         console.log(assetData);
         expect(assetData).toBeDefined();
     });
     test("Test: Get asset data from asset ESW00:CME_EMINIS (future).", async () => {
-        const googleFinance = new googlefinance_1.default();
         const assetData = await googleFinance.getAssetData("ESW00:CME_EMINIS");
         console.log(assetData);
         expect(assetData).toBeDefined();
     });
     test("Test: Convert $120 to Japanese Yen.", async () => {
-        const googleFinance = new googlefinance_1.default();
         const conversion = await googleFinance.convertCurrency("USD", 120, "JPY");
         console.log(conversion);
         expect(conversion).toBeDefined();
     });
     test("Test: Get the top stories from Google Finance's home page.", async () => {
-        const googleFinance = new googlefinance_1.default();
         const stories = await googleFinance.getTopStories();
         console.log(stories);
         expect(stories).toBeDefined();
